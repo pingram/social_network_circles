a524fc54df901b64775ffc172d4143e65150375b
Philip Ingram <philip.d.ingram@gmail.com>
2014-04-28 10:43:33 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+AaPlus::Application.config.secret_key_base = '5ad893c3f539ec60ba42f1e7db94261d23bd5154e17bbbd9a8717e437b098339214caa6b222781e4e16f51e44b22a5860d965f1a6d331957f163a266266db1cb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+AaPlus::Application.config.session_store :cookie_store, key: '_aa_plus_session'
