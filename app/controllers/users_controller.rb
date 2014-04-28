f5b39041bcc8f087a98d327690b06256597dbca1
Philip Ingram <philip.d.ingram@gmail.com>
2014-04-28 16:40:36 -0700

    sign up with posts

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,15 @@
 class UsersController < ApplicationController
   def new
     @user = User.new
+    @post = Post.new
+    3.times { @post.links << Link.new }
   end
 
   def create
     @user = User.new( user_params )
+    @post = @user.posts.build( post_params )
+    @post.links.build( link_params )
+
 
     if @user.save
       sign_in!(@user)
@@ -18,6 +23,18 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:email, :password)
+    params.require(:user).permit(:email, :password, :posts)
   end
+
+  def post_params
+    params.require(:post).permit(:body)
+  end
+
+  def link_params
+    params.permit( :links => [:title, :url] )
+      .require(:links)
+      .values
+      .reject { |link| link.values.all?(&:empty?) }
+  end
+
 end
