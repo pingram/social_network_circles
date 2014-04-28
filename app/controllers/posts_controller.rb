20af564eb7a5255021bbb9cc742b56ffa9038b85
Philip Ingram <philip.d.ingram@gmail.com>
2014-04-28 15:06:26 -0700

    posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,16 +2,19 @@ class PostsController < ApplicationController
   def new
     set_friend_circles
     @post = Post.new
+    3.times { @post.links << Link.new }
   end
 
   def create
     set_friend_circles
     @post = Post.new(post_params)
     @post.user_id = current_user.id
+    @post.links.new( link_params )
 
     if @post.save
       redirect_to posts_url
     else
+      (3 - @post.links.length).times { @post.links << Link.new }
       flash.now[:errors] = @post.errors.full_messages
       render :new
     end
@@ -36,6 +39,13 @@ class PostsController < ApplicationController
 
   def set_friend_circles
     @friend_circles = current_user.owned_circles
+  end
+
+  def link_params
+    params.permit( :links => [:title, :url] )
+      .require(:links)
+      .values
+      .reject { |link| link.values.all?(&:empty?) }
   end
 
   def post_params
--- app/models/link.rb
+++ app/models/link.rb
@@ -1,2 +1,5 @@
 class Link < ActiveRecord::Base
+  validates :title, :url, presence: true
+
+  belongs_to :post, inverse_of: :links
 end
